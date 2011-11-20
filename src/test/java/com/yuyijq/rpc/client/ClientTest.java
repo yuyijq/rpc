1c19bfbb389e6f6dc30fa229cd37155b4cb3d58a
Zhaohui Yu <zhaohuiyu@Zhaohuis-MacBook-Air.local>
2011-11-20 22:53:58 +0800

    [zhaohui.yu] Added client test.

--- src/test/java/com/yuyijq/rpc/client/ClientTest.java
+++ src/test/java/com/yuyijq/rpc/client/ClientTest.java
@@ -3,8 +3,12 @@ package com.yuyijq.rpc.client;
 import com.yuyijq.rpc.User;
 import com.yuyijq.rpc.UserService;
 import com.yuyijq.rpc.client.netty.NettyRpcTransport;
+import org.junit.Test;
 
 import java.net.InetSocketAddress;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 /**
  * User: zhaohuiyu
@@ -12,21 +16,22 @@ import java.net.InetSocketAddress;
  * Time: 10:46 AM
  */
 public class ClientTest {
-    public static void main(String[] args) {
+    @Test
+    public void should_call_remote_rpc_server() {
         RpcTransport transport = new NettyRpcTransport();
-        transport.connect(new InetSocketAddress("127.0.0.1",9006));
+        transport.connect(new InetSocketAddress("127.0.0.1", 9006));
         ChannelFactory channelFactory = new ChannelFactory(transport);
         ClientFactory clientFactory = new ClientFactory(new ClientProxy(channelFactory));
         UserService service = clientFactory.getService(UserService.class);
 
         User user = service.findById(1L);
 
-        System.out.println(user.getUserName());
+        assertThat(user.getUserName(), is("yuyijq"));
 
-        service.beginFindById(1L,new Callback<User>(){
-           public void receive(User user){
-               System.out.println(user.getUserName());
-           }
+        service.beginFindById(1L, new Callback<User>() {
+            public void receive(User user1) {
+                assertThat(user1.getUserName(), is("yuyijq"));
+            }
         });
     }
 }
